1ac9811f7432ded74192b94fd879290a81becf30
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-24 00:54:19 +0200

    End of canary

--- carsharing/carsharing.py
+++ carsharing/carsharing.py
@@ -9,24 +9,24 @@ if __name__ == "__main__":
 app = FastAPI()
 
 
-@app.get("/")
-def welcome():
-    """Return a friendly welcome message."""
-    return {'message': "Welcome to the Car Sharing service!"}
-
-
-@app.get("/date")
-def date():
-    """Return the current date/time."""
-    return {'date': datetime.now()}
-
-
-# request parameter
-# localhost:8000/greeting/?name=John
-@app.get("/greeting")
-def welcome(name):
-    """Return a friendly welcome message."""
-    return {"message": f'Hi {name}. Welcome to the Car Sharing service!'}
+# @app.get("/")
+# def welcome():
+#     """Return a friendly welcome message."""
+#     return {'message': "Welcome to the Car Sharing service!"}
+#
+#
+# @app.get("/date")
+# def date():
+#     """Return the current date/time."""
+#     return {'date': datetime.now()}
+#
+#
+# # request parameter
+# # localhost:8000/greeting/?name=John
+# @app.get("/greeting")
+# def welcome(name):
+#     """Return a friendly welcome message."""
+#     return {"message": f'Hi {name}. Welcome to the Car Sharing service!'}
 
 
 db = [
--- carsharing/test/test_canary.py
+++ carsharing/test/test_canary.py
@@ -8,24 +8,24 @@ from carsharing import app
 client = TestClient(app)
 
 
-def test_welcome():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "Welcome" in response.text
-
-
-def test_date():
-    response = client.get("/date")
-    assert response.status_code == 200
-    # without exception it asserts date is a valid format
-    datetime.fromisoformat(json.loads(response.text)["date"])
-
-
-def test_greeting():
-    name = "John"
-    expected = f"Hi {name}. Welcome to the Car Sharing service!"
-    response = client.get(f"/greeting/?name={name}")
-    result = json.loads(response.text)[
-        "message"]
-    assert response.status_code == 200
-    assert expected in result
+# def test_welcome():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert "Welcome" in response.text
+#
+#
+# def test_date():
+#     response = client.get("/date")
+#     assert response.status_code == 200
+#     # without exception it asserts date is a valid format
+#     datetime.fromisoformat(json.loads(response.text)["date"])
+#
+#
+# def test_greeting():
+#     name = "John"
+#     expected = f"Hi {name}. Welcome to the Car Sharing service!"
+#     response = client.get(f"/greeting/?name={name}")
+#     result = json.loads(response.text)[
+#         "message"]
+#     assert response.status_code == 200
+#     assert expected in result
