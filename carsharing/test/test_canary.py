03c68e501569c89c5b4b5a18a768a085c9a82d9d
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-23 13:09:19 +0200

    Initial commit

---
+++ carsharing/test/test_canary.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from carsharing import app
+
+from datetime import datetime
+
+import json
+
+client = TestClient(app)
+
+
+def test_welcome():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "Welcome" in response.text
+
+
+def test_date():
+    response = client.get("/date")
+    assert response.status_code == 200
+    # assert date is a valid format
+    datetime.fromisoformat(json.loads(response.text)["date"])
+
