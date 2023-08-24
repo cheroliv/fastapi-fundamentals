82ffa6975c0b2c9014e0f6933d6dcf8af61979e5
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-24 05:01:54 +0200

    clean

--- carsharing/test/test_canary.py
+++ carsharing/test/test_canary.py
@@ -16,10 +16,23 @@ def welcome():
     return {'message': "Welcome to the Car Sharing service!"}
 
 
+def test_welcome():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "Welcome" in response.text
+
+
 @canary.get("/date")
 def date():
     """Return the current date/time."""
     return {'date': datetime.now()}
+
+
+def test_date():
+    response = client.get("/date")
+    assert response.status_code == 200
+    # without exception it asserts date is a valid format
+    datetime.fromisoformat(json.loads(response.text)["date"])
 
 
 # request parameter
@@ -30,20 +43,7 @@ def welcome(name):
     return {"message": f'Hi {name}. Welcome to the Car Sharing service!'}
 
 
-def test_welcome():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "Welcome" in response.text
-
-
-def test_date():
-    response = client.get("/date")
-    assert response.status_code == 200
-    # without exception it asserts date is a valid format
-    datetime.fromisoformat(json.loads(response.text)["date"])
-
-
-def test_greeting():
+def test_greeting_with_query_param():
     name = "John"
     expected = f"Hi {name}. Welcome to the Car Sharing service!"
     response = client.get(f"/greeting/?name={name}")
